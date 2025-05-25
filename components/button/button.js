7f076e7ef88c205caded34845bd00d8064a1d774
bkare <18614629+bkare@users.noreply.github.com>
2025-05-25 15:36:36 +0300

    Ripple efekti ve uyarı mesajı birlikte çalışan veriyon eklendi

--- components/button/button.js
+++ components/button/button.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll(".btn");
+    const buttons = document.querySelectorAll(".btn:not(.btn-disabled");
 
-    buttons.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            if (btn.disabled) return;
-            alert(`"${btn.textContent}" butonuna tıkladınız.`); 
+    buttons.forEach((btn => {
+        btn.addEventListener("click", (e) => {
+            //ripple efektini oluştur
+            const ripple = document.createElement("span");
+            ripple.classList.add("ripple");
+
+            //tıklama koordinatlarını al
+            const rect = btn.getBoundingClientRect();
+            ripple.style.left= `${e.clientX - rect.left}px`;
+            ripple.style.top = `${e.clientY - rect.top}px`;
+
+            //butona ekle ve kaldır
+            btn.appendChild(ripple);
+            setTimeout(() => ripple.remove(), 600);
+
+            //uyarı mesajı
+            alert(`"${btn.textContent.trim()}" butonuna tıkladınız.`);
         });
-    });
+    }));
 });
